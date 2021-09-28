9d72ff72c7deee91f3e8656d76c62345450739a7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-09-29 08:03:00 +0900

    Update HackerRank-in-a-String-.cpp

--- HackerRank-in-a-String-.cpp
+++ HackerRank-in-a-String-.cpp
@@ -1,1 +1,18 @@
+string hackerrankInString(string s)
+{
+    std::deque<char> dq = {'h', 'a', 'c', 'k', 'e', 'r', 'r', 'a', 'n', 'k'};
+    
+    for (const auto & c : s)
+    {
+        if (c == dq[0])
+        {
+            dq.pop_front();
+            if (dq.empty())
+            {
+                return "YES";
+            }
+        }
+    }
 
+    return "NO";
+}
