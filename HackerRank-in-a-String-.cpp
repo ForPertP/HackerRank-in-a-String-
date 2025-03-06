ffe00560be3e5401882bb45fdf7969d5ccae4518
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-06 17:08:44 +0900

    Update HackerRank-in-a-String-.cpp

--- HackerRank-in-a-String-.cpp
+++ HackerRank-in-a-String-.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
 string hackerrankInString(string s)
 {
     std::deque<char> dq = {'h', 'a', 'c', 'k', 'e', 'r', 'r', 'a', 'n', 'k'};
