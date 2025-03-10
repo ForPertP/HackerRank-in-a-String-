8989f47fbc695a13ce89f49f01194d9c8ad83e38
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-11 00:56:24 +0900

    Update HackerRank-in-a-String-.cs

--- HackerRank-in-a-String-.cs
+++ HackerRank-in-a-String-.cs
@@ -1,1 +1,24 @@
-HackerRank-in-a-String-
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+}
