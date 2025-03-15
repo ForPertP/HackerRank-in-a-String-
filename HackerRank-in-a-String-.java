4acb14bdf24762d5ced000bcbfd3b5b180ac59b2
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-03-16 02:00:04 +0900

    Update HackerRank-in-a-String-.java

--- HackerRank-in-a-String-.java
+++ HackerRank-in-a-String-.java
@@ -20,3 +20,30 @@ class Result {
      */
 
 }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.hackerrankInString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
